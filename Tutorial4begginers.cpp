558bf4edc755b7527f4bdecebc96b9f2cd96b1d1
Kiam11 <90165991+Kiam11@users.noreply.github.com>
2021-09-06 18:07:50 -0300

    Add files via upload

---
+++ Tutorial4begginers.cpp
@@ -0,0 +1,75 @@
+// -Drawing a shape-
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout << "     /|" << endl;
+//     cout << "    / |" << endl;
+//     cout << "   /  |" << endl;
+//     cout << "  /   |" << endl;
+//     cout << " /    |" << endl;
+//     cout << "/_____|" << endl;
+//     return 0;
+// }
+
+
+
+// -variables-
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string CN = "John";
+//     int CA = 35;
+//     cout << "There once was a man named " << CN << endl;
+//     cout << "He was " << CA << " years old" << endl;
+//     cout << "He liked the name " << CN << endl;
+//     cout << "But didnt liked being " << CA << endl;
+//     return 0;
+// }
+
+
+
+//-data types-
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     //examples are the variables 
+//     string example1 = "Hello World"; 
+//     int example2 = 119113;
+//     char example3 = 'K';
+//     float example4 = 1.234567; //extra
+//     double example5 = 1.23456789012345;
+//     bool example6 = true;
+//     cout << example1 << example2 << example3 << example4 << example5 << example6 << endl;
+//     return 0;
+// }
+
+
+
+
+//-Working with strings-
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string example = "Hello World";
+//     cout << example.length() << endl;
+//     cout << example[0] << endl;
+//     example[0] = 'B';
+//     cout << example << endl;
+//     cout << example.find("l") << endl;
+//     cout << example.substr(3,5) << endl;
+//     return 0;
+// }
+
+
+
+//-Working with numbers-
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main(){
+//     int example = 8.0;
+//     cout << example / 3.0 << endl;
+//     example++; //or --
+//     cout << example % 3 << endl;
+//     return 0;
+// }
